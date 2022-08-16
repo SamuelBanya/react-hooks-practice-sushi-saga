7aa258d64ef7e78655b735ae86bc235ccd55976c
Samuel Banya <sbanya@fastmail.com>
2022-08-15 21:45:55 -0400

    Changing API endpoints to be Heroku based

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
-const API = "http://localhost:3001/sushis";
+const API = "https://sushi-saga-jsonserver.herokuapp.com/sushis";
 
 function App() {
   const [sushis, setSushis] = useState([]);
